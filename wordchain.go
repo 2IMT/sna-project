1c89a774b42febc8b63749f3601e4cd3a86dd749
2IMT <daniil_t04@mail.ru>
2024-04-22 14:43:03 +0300

    Format wordchain.go

--- wordchain.go
+++ wordchain.go
@@ -2,13 +2,13 @@ package main
 
 import "log"
 
-var Env Environment;
+var Env Environment
 
 func main() {
-    Env, err := LoadEnvironment();
-    if err != nil {
-        log.Fatalf("Failed to load environment: %s\n", err);
-    }
+	Env, err := LoadEnvironment()
+	if err != nil {
+		log.Fatalf("Failed to load environment: %s\n", err)
+	}
 
-    log.Printf("Environment: %+v", Env);
+	log.Printf("Environment: %+v", Env)
 }
