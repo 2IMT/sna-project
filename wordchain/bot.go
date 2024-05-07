d0e8cc157bdfab4f42ca09dbf7c2fa52effbdc2f
AliiaB <a.bogapova@innopolis.university>
2024-05-07 17:30:43 +0300

    connection of 2 players by using /play command

--- wordchain/bot.go
+++ wordchain/bot.go
@@ -3,10 +3,16 @@ package main
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
     "log"
+	"sync"
 )
 
 type Bot struct {
 	api *tgbotapi.BotAPI
+	gameQueue []int64
+	connectedPlayers map[int64]int64
+	mu sync.Mutex
+	player1          int64
+	player2          int64
 }
 
 type Message struct {
@@ -24,6 +30,8 @@ func NewBot() (Bot, error) {
 		return Bot{}, err
 	}
 
+	result.connectedPlayers = make(map[int64]int64)
+
 	return result, nil
 }
 
@@ -36,11 +44,38 @@ func (b Bot) SendMessage(chatId int64, text string) {
 
 func (b Bot) GetMessageChan() chan Message {
 	result := make(chan Message)
-	go handleUpdates(b.api, result)
+	go handleUpdates(b.api, result, b.connectedPlayers)
 	return result
 }
 
-func handleUpdates(api *tgbotapi.BotAPI, msgChan chan Message) {
+func (b *Bot) AddToGameQueue(chatId int64){
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.gameQueue = append(b.gameQueue, chatId)
+	if len(b.gameQueue)>=2{
+		b.player1=b.gameQueue[0]
+		b.player2=b.gameQueue[1]
+		b.gameQueue=b.gameQueue[2:]
+
+		b.connectedPlayers[b.player1]=b.player2
+		b.connectedPlayers[b.player2]=b.player1
+
+		b.SendMessage(b.player1, "You are now connected with aeaeaea")
+		b.SendMessage(b.player2, "You are now connected with sdkdkja")
+	}
+}
+
+func(b *Bot) ForwardMessage(chatId int64, text string){
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if opponent, ok := b.connectedPlayers[chatId]; ok {
+		b.SendMessage(opponent, text)
+	}
+}
+
+func handleUpdates(api *tgbotapi.BotAPI, msgChan chan Message, connectedPlayers map[int64]int64) {
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = 30
 	updatesChan := api.GetUpdatesChan(updateConfig)
@@ -51,7 +86,7 @@ func handleUpdates(api *tgbotapi.BotAPI, msgChan chan Message) {
 		}
 
 		chatId := update.Message.Chat.ID
-		username := update.Message.Chat.UserName
+		//username := update.Message.Chat.UserName
 
 		var text string
 		if len(update.Message.Text) != 0 {
@@ -62,6 +97,10 @@ func handleUpdates(api *tgbotapi.BotAPI, msgChan chan Message) {
 			continue
 		}
 
-		msgChan <- Message{chatId, username, text}
+		if text=="/play"{
+			B.AddToGameQueue(chatId)
+		}else {
+            B.ForwardMessage(chatId, text)
+        }
 	}
 }
